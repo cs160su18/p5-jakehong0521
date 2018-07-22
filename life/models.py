fef8b1a350645c2278714a9264990bb9b5cd5b0a
jakehong0521 <jakehong0521@berkeley.edu>
2018-07-22 00:41:32 -0400

    setup

--- life/models.py
+++ life/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
 class Group(models.Model):
-	established = models.DateTimeField(auto_now_add=True)
-	name = models.CharField(max_length=50)
+  established = models.DateTimeField(auto_now_add=True)
+  name = models.CharField(max_length=50)
+  
+class Item(models.Model):
+  name = models.CharField(max_length=50)
+  date = models.DateTimeField(auto_now_add=True)
+  description = models.CharField(max_length=200)
+
+class User(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=10)
